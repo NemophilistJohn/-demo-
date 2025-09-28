3f6eeb86163a16d9e41f36efa6de365aa1e007c9
John Wayne <88379642+NemophilistJohn@users.noreply.github.com>
2025-09-28 16:59:36 +0800

    Update config.py

---
+++ taobaoSpider/taobao_price_monitor/config/config.py
@@ -0,0 +1,8 @@
+# 邮件配置
+EMAIL_CONFIG = {
+    'smtp_server': 'smtp.qq.com', 
+    'smtp_port': 587,           
+    'sender': '663039825@qq.com',
+    'password': 'lhhroxylhnqdbfhf',
+    'receivers': ['yourspeter@foxmail.com']
+}
